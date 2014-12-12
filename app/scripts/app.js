071d2cb8fab9d3eca6b26c76ac062fefbe9030a7
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-12-12 13:13:44 +0100

    Initial commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -52,13 +52,16 @@ function formatTilFraNavn(AVSMOT_OJ){
   }
 }
 
-
 var JournalOrderForm = React.createClass({
   getInitialState: function(){
     return {
       showOrderForm:'hideOrderForm',
       buttonTitle: 'Bestill innsyn',
-      innsynBestilt: ''
+      innsynBestilt: '',
+      bestillernavn:'',
+      bestillermail:'',
+      bestilleraddress:'',
+      errormsg:''
     }
   },
   buttonHandler: function(e){
@@ -68,24 +71,47 @@ var JournalOrderForm = React.createClass({
   },
   orderHandler: function(e){
     e.preventDefault();
-    this.setState({showOrderForm:'hideOrderForm', buttonTitle: 'Innsyn bestilt'});
+    var that = this;
+    var mail = {
+      subject:'Innsynsbestilling: ' + this.props.doknr + ' ' + this.props.doktittel,
+      from: this.state.bestillermail,
+      content: this.state.bestillernavn + '\n'+ this.state.bestilleraddress
+    };
+
+    $.get('http://ws.t-fk.no/mail.php', mail, function(data){
+      if(data.results.errorcode === '0'){
+        that.setState({showOrderForm:'hideOrderForm', buttonTitle: 'Innsyn bestilt', bestillernavn:'', bestillermail:'', bestilleraddress:''});
+      } else {
+        var errormsg = 'Noe gikk galt, vennligst kontakt arkivet på <a href="mailto:post.arkiv@t-fk.no">post.arkiv@t-fk.no</a>';
+        that.setState({showOrderForm:'hideOrderForm', buttonTitle: 'Bestill innsyn', errormsg: errormsg});
+      }
+    });
+
+  },
+  changeHandlerName: function(e){
+    this.setState({bestillernavn : e.target.value});
+  },
+  changeHandlerMail: function(e){
+    this.setState({bestillermail : e.target.value});
+  },
+  changeHandlerAddress: function(e){
+    this.setState({bestilleraddress : e.target.value});
   },
   render: function(){
     return (
       <div>
-    <button className="button--secondary" onClick={this.buttonHandler}>{this.state.buttonTitle}</button>
+    <button className="button--secondary" onClick={this.buttonHandler}>{this.state.buttonTitle}</button><span className="orderFormError">{this.state.errormsg}</span>
       <div className={this.state.showOrderForm}>
         <h3>Innsynsbestilling</h3>
-        <form>
-          <input type="hidden" name="journalpost" value={this.props.doknr + ' ' + this.props.doktittel} />
+        <form onSubmit={this.orderHandler}>
           <fieldset>
             <legend>Dokument: {this.props.doknr} {this.props.doktittel}</legend>
-          <label htmlFor="bestillersnavn">Navn: </label>
-            <input type="text" name="bestillersnavn" placeholder="Fullt navn" autoComplete="name" />
-            <label htmlFor="bestillersepost">E-post: </label>
-            <input type="text" name="bestillerepost" placeholder="E-post" autoComplete="email" />
-            <label htmlFor="bestilleradresse">Postadresse: </label>
-            <input type="text" name="bestilleradresse"/>
+          <label htmlFor="bestillernavn">Navn: </label>
+            <input type="text" name="bestillernavn" placeholder="Fullt navn" autoComplete="name" onChange={this.changeHandlerName} />
+            <label htmlFor="bestillermail">E-post: </label>
+            <input type="text" name="bestillermail" placeholder="E-post" autoComplete="email" onChange={this.changeHandlerMail} />
+            <label htmlFor="bestilleraddress">Postadresse: </label>
+            <input type="text" name="bestilleraddress" onChange={this.changeHandlerAddress} />
           </fieldset>
         </form>
           <p>
